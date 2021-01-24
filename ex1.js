fceb5055311eb52d0dd37114dc3ed534a55676d5
Yaniv Bahalker <ybahalker@gmail.com>
2021-01-24 13:00:56 +0200

    Test git

--- ex1.js
+++ ex1.js
@@ -1,6 +1,6 @@
 (function () {
     'use strict';
-
+/* test */
     const input = require('readline-sync');
     let name = input.question("Please enter your name?\n")
     console.log("Hello " + name + "!")
